77f846adf4be046dfc9e7966af4a77ca451b5523
Jan Rembold <jan.rembold@virtual-identity.com>
2018-01-29 15:24:44 +0100

    removed unused delete tasks

--- tasks/clean.js
+++ tasks/clean.js
@@ -4,24 +4,12 @@ const config = require('./../config');
 
 gulp.task('clean:dev', function() {
   return del([
-      config.devDir + '/**/*'
+      config.devDir + '/**'
   ]);
 });
 
 gulp.task('clean:dist', function() {
   return del([
-      config.distDir + '/**/*'
+      config.distDir + '/**'
   ]);
-});
-
-gulp.task('clean:resources:dev', function() {
-    return del([
-        config.devDir + '/*/resources/**'
-    ]);
-});
-
-gulp.task('clean:resources:dist', function() {
-    return del([
-        config.distDir + '/*/resources/**'
-    ]);
 });
